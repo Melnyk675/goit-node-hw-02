a4ba7c1cc500246f5757e2ee026adbaea8e08ab6
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2023-07-05 20:33:31 +0300

    commit

--- middlewares/HttpError.js
+++ middlewares/HttpError.js
@@ -1,6 +1,8 @@
-const HttpError = (status, message) => {
-    const error = new Error(message);
-    error.status = status;
-    return error;
-  };
-  module.exports = HttpError;
+class HttpError extends Error {
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+  }
+}
+
+module.exports = HttpError;
